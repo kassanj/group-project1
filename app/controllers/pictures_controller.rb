2851642f0d46e4c18dd80c836cfc820cd8429090
Kassandra M <kassandram022@gmail.com>
2015-02-13 13:51:32 -0500

    css edits, login user edits, profile edits

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -69,7 +69,7 @@ class PicturesController < ApplicationController
   def destroy
     @picture.destroy
     respond_to do |format|
-      format.html { redirect_to pictures_url, notice: 'Video was successfully destroyed.' }
+      format.html { redirect_to pictures_url, notice: 'Video was successfully deleted.' }
       format.json { head :no_content }
     end
   end
